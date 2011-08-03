9468f227ad226e270cb9a3f2ca0447f8c1b83c86
Florian Gilcher <florian.gilcher@asquera.de>
2011-08-03 12:48:44 +0200

    Tiny script using ESExecutors

--- lib/script.rb
+++ lib/script.rb
@@ -5,18 +5,29 @@ Bundler.setup
 require 'echolon'
 
 class JRubyRiverModule
+  include_class "org.elasticsearch.common.util.concurrent.EsExecutors"
+
   def initialize(riverName, settings, client, threadPool)
     @riverName  = riverName
     @settings   = settings
     @client     = client
     @threadPool = threadPool
   end
-  
+
   def start
-    puts "start"
+    @test_thread = EsExecutors.daemonThreadFactory(@settings.globalSettings, "test_thread").newThread do
+      while(true && !@closed) do 
+        puts "start"
+	sleep 0.1
+      end 
+    end
+    @test_thread.start
   end
-  
+
   def close
-    puts "stop"
+    unless @closed
+      @test_thread.interrupt
+      @closed = true
+    end
   end
 end
